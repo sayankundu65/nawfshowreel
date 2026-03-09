4a30b848a380d818f11b959af448191d60af048a
sayankundu65 <sayankundu65@gmail.com>
2026-03-10 00:55:38 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -94,7 +94,7 @@ export default function App() {
               <div className={`relative w-full ${isFullscreen ? 'h-full' : 'pt-[56.25%]'}`}>
                 <iframe
                   className={`absolute top-0 left-0 w-full h-full ${isFullscreen ? 'object-contain' : ''}`}
-                  src="https://www.youtube.com/embed/gIJ-OCGRdhg?autoplay=0&controls=1&rel=0&modestbranding=1"
+                  src="https://www.youtube.com/embed/AbFICVpYEAc?autoplay=0&controls=1&rel=0&modestbranding=1"
                   title="YouTube video player"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
